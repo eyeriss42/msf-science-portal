f1bbb98197ff727bff8b42dbe865b464a6ab41e4
eyeriss42 <iris.mck@pm.me>
2024-11-05 17:48:59 -0500

    move title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import styles from "./page.module.css";
 import CarouselComponent from "./components/CarouselComponent";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { Container } from 'react-bootstrap';
+
 export default function Home() {
   return (
-    <div>
-      <h2>Science Portal Take Home</h2>
+    <Container>
+      <h2 className="mt-4 ms-3">Science Portal Take Home</h2>
       <div className='padding-container'>
-      <CarouselComponent />
+        <CarouselComponent />
       </div>
-      </div>
+    </Container>
+  );
+}
+
 
     // <div className={styles.page}>
     //   <main className={styles.main}/>
@@ -18,5 +24,3 @@ export default function Home() {
     //   <footer className={styles.footer}>
     //   </footer>
     // </div>
-  );
-}
